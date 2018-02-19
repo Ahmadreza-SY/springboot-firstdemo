af604560096e650582fca20c0bceff42dff0e723
Ahmadreza-SY <ahmadreza.saboor2012@gmail.com>
2018-02-19 19:06:34 +0330

    initial commit

---
+++ src/main/kotlin/ir/tapsell/firstdemo/FirstdemoApplication.kt
@@ -0,0 +1,11 @@
+package ir.tapsell.firstdemo
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class FirstdemoApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(FirstdemoApplication::class.java, *args)
+}
